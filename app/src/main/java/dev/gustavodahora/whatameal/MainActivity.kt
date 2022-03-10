23e3bf46e8e6a5f1bf3360d4b0c26152391659ea
Gustavo da Hora <gustavo.carmodahora@outlook.com>
2022-03-09 21:24:43 -0300

    Adding new model and layout res

--- app/src/main/java/dev/gustavodahora/whatameal/MainActivity.kt
+++ app/src/main/java/dev/gustavodahora/whatameal/MainActivity.kt
@@ -1,11 +1,29 @@
 package dev.gustavodahora.whatameal
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var appLogo: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupViews()
+        setupClickEvents()
+    }
+
+    private fun setupViews() {
+        appLogo = findViewById(R.id.app_logo)
+    }
+
+    private fun setupClickEvents() {
+        appLogo.setOnClickListener {
+            Toast.makeText(applicationContext, getString(R.string.this_is_a_soup), Toast.LENGTH_LONG).show()
+        }
     }
 }
